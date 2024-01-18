1be9fe49c75e2f6e487072565f415a7aa72f7506
pmagar9564 <pmagar@flemingcollege.ca>
2024-01-18 00:20:41 -0500

    Added comment to script1.py

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
+#Print lines from Zen of Python 
 print("Sparse is better than dense.")
 print("Beautiful is better than ugly.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+#Print lines from Zen of Python 
 print("Readibility counts.")
 print("In the face of ambiguity, refuse the temptation to guess.")
