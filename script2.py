94f9397ea359a64b9e400fed42c75ff733110193
pmagar9564 <pmagar@flemingcollege.ca>
2024-01-18 01:01:39 -0500

    Modified both files in the repo clone
    -Deleted a print() statements from script1.py
    -Added two print() statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,5 @@
 #Print lines from Zen of Python
 print("Readibility counts.")
 print("In the face of ambiguity, refuse the temptation to guess.")
+print("Simple is better than complex.")
+print("Now is better than never.")
